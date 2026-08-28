13c866f209b21bb98afaca73553e8611a60a5243
Victor <50677889+zzvictoor@users.noreply.github.com>
2026-08-28 12:59:21 +0100

    refactor: modernize MySQL connection setup

--- Timeline/Database/__init__.py
+++ Timeline/Database/__init__.py
@@ -1,63 +1,115 @@
-"""
-Timeline's DB System
-"""
-from Timeline.Server.Constants import TIMELINE_LOGGER
-from Timeline.Database.DB import Penguin
+"""Timeline database registry and validation helpers."""
 
 from collections import deque
 import logging
 
-
-from twisted.enterprise import adbapi
 from twistar.registry import Registry
 from twistar.dbconfig.mysql import ReconnectingMySQLConnectionPool
 
-from Timeline.Database.DB import Penguin, Igloo, Avatar, Currency, Ninja, Asset, Ban, CareItem, Friend, Request, \
-    Ignore, Inventory, Mail, Membership, MusicTrack, Puffle, Stamp, StampCover, IglooFurniture, IglooLike, Coin
+from Timeline.Database.DB import (
+    Asset,
+    Avatar,
+    Ban,
+    CareItem,
+    Coin,
+    Currency,
+    Friend,
+    Igloo,
+    IglooFurniture,
+    IglooLike,
+    Ignore,
+    Inventory,
+    Mail,
+    Membership,
+    MusicTrack,
+    Ninja,
+    Penguin,
+    Puffle,
+    Request,
+    Stamp,
+    StampCover,
+)
+from Timeline.Server.Constants import TIMELINE_LOGGER
+
 
 class DBManagement(object):
-    
-    def __init__(self, user, passd, db):
+    def __init__(self, user, passd, db, host="127.0.0.1", port=3306):
         self.db_data = (user, db)
+        self.host = host
+        self.port = int(port)
         self.logger = logging.getLogger(TIMELINE_LOGGER)
         self.conn = False
-        
+
         if self.setupRegistry(passd):
-            self.logger.info("MySQL Database pool setup successfully")
+            self.logger.info(
+                "MySQL database pool setup successfully @ %s:%s/%s",
+                self.host,
+                self.port,
+                db,
+            )
         else:
-            self.logger.info("Unable to setup MySQL Pool")
-        
+            self.logger.error("Unable to setup MySQL pool")
+
     def setupRegistry(self, passd):
         user, db = self.db_data
-        self.logger.info("Starting MySQL DB Pool... @{0}:{1}".format(*self.db_data))
+        self.logger.info("Starting MySQL DB pool @ %s:%s/%s", self.host, self.port, db)
         try:
-            Registry.register(Penguin, Igloo, Avatar, Currency, Ninja, Asset, Ban, CareItem, Friend, Request, Ignore,
-                              Inventory, Mail, Membership, MusicTrack, Puffle, Stamp, StampCover, Coin)
+            Registry.register(
+                Penguin,
+                Igloo,
+                Avatar,
+                Currency,
+                Ninja,
+                Asset,
+                Ban,
+                CareItem,
+                Friend,
+                Request,
+                Ignore,
+                Inventory,
+                Mail,
+                Membership,
+                MusicTrack,
+                Puffle,
+                Stamp,
+                StampCover,
+                Coin,
+            )
             Registry.register(Igloo, IglooFurniture, IglooLike)
 
-            Registry.DBPOOL = ReconnectingMySQLConnectionPool('MySQLdb', user=user, passwd=passd, db=db, cp_reconnect=True)
+            Registry.DBPOOL = ReconnectingMySQLConnectionPool(
+                "MySQLdb",
+                host=self.host,
+                port=self.port,
+                user=user,
+                passwd=passd,
+                db=db,
+                charset="utf8mb4",
+                use_unicode=True,
+                cp_reconnect=True,
+            )
             self.conn = True
-    
-        except Exception, e:
-            self.logger.error("Unable to start MySQL Pool on given details. (E:{0})".format(e))
+        except Exception as exc:
+            self.logger.exception("Unable to start MySQL pool: %s", exc)
             self.conn = False
-        
+
         return self.conn
-        
+
 
 def validateNickname(peng):
     peng.nickname = peng.nickname.strip()
-    n = peng.nickname
-    m = n.replace(' ', '')
-    
-    if len(n) > 20: 
-        peng.errors.add('nickname', "Nickname should be less than 21 characters")
-    
-    if not m.isalnum():
-        peng.errors.add('nickname', "Nickname should be alpha numeric")
+    nickname = peng.nickname
+    compact = nickname.replace(" ", "")
+
+    if len(nickname) > 20:
+        peng.errors.add("nickname", "Nickname should be less than 21 characters")
+
+    if not compact.isalnum():
+        peng.errors.add("nickname", "Nickname should be alpha numeric")
+
 
 def validateInventory(peng):
-    peng.inventory = peng.inventory.strip('%')
-        
+    peng.inventory = peng.inventory.strip("%")
+
 
 Penguin.addValidator(validateNickname)
