eb81dc35c368f79b2f709267667147de98ed4590
Victor <50677889+zzvictoor@users.noreply.github.com>
2026-08-28 13:13:19 +0100

    fix: port Card Jitsu Fire starter deck handler to Python 3

--- Timeline/Handlers/Games/CardJitsuFire/CardJitsu.py
+++ Timeline/Handlers/Games/CardJitsuFire/CardJitsu.py
@@ -1,71 +1,90 @@
-from Timeline.Server.Constants import TIMELINE_LOGGER, LOGIN_SERVER, WORLD_SERVER, FIRE_STARTER_DECK
-from Timeline import Username, Password, Inventory
-from Timeline.Utils.Events import Event, PacketEventHandler, GeneralEvent
+"""Card Jitsu Fire handlers."""
+
+import logging
+from random import choice
+
+from twisted.internet.defer import inlineCallbacks
+
+from Timeline.Handlers.Games.CardJitsu.CardJitsu import getSensei
 from Timeline.Handlers.Games.CardJitsuFire import CJ_MATS, CJMat
 from Timeline.Handlers.Games.CardJitsuFire.Sensei import CardJitsuFireSenseiGame
-from Timeline.Handlers.Games.CardJitsu.CardJitsu import getSensei
-
-from twisted.internet.defer import inlineCallbacks, returnValue
-
-from collections import deque
-import logging
-from time import time
-from random import choice
+from Timeline.Server.Constants import FIRE_STARTER_DECK, TIMELINE_LOGGER, WORLD_SERVER
+from Timeline.Utils.Events import GeneralEvent, PacketEventHandler
 
 logger = logging.getLogger(TIMELINE_LOGGER)
 
-@GeneralEvent.on('Room-handler')
-def setCJMats(ROOM_HANDLER):
-    for i in CJ_MATS:
-        ROOM_HANDLER.ROOM_CONFIG.WADDLES[i] = CJMat(ROOM_HANDLER, i, "FireJitsuMat", "Card Jitsu Fire Mat", CJ_MATS[i], False, False, None)
-        ROOM_HANDLER.ROOM_CONFIG.WADDLES[i].waddle = i
-        ROOM_HANDLER.ROOM_CONFIG.WADDLES[i].waddles = CJ_MATS[i]
+
+@GeneralEvent.on("Room-handler")
+def setCJMats(room_handler):
+    for mat_id in CJ_MATS:
+        room_handler.ROOM_CONFIG.WADDLES[mat_id] = CJMat(
+            room_handler,
+            mat_id,
+            "FireJitsuMat",
+            "Card Jitsu Fire Mat",
+            CJ_MATS[mat_id],
+            False,
+            False,
+            None,
+        )
+        room_handler.ROOM_CONFIG.WADDLES[mat_id].waddle = mat_id
+        room_handler.ROOM_CONFIG.WADDLES[mat_id].waddles = CJ_MATS[mat_id]
 
     logger.debug("Card Jitsu Fire Initiated")
 
-@PacketEventHandler.onXT('z', 'jsen', WORLD_SERVER, p_r = False)
-def handleJoinSenseiCJ(client, data):
-    ROOM_HANDLER = client.engine.roomHandler
 
-    if client['room'].ext_id != 953:
+@PacketEventHandler.onXT("z", "jsen", WORLD_SERVER, p_r=False)
+def handleJoinSenseiCJ(client, data):
+    room_handler = client.engine.roomHandler
+    if client["room"].ext_id != 953:
         return
 
     if 997 not in CJ_MATS:
         CJ_MATS[997] = 2
 
-    gameMat = CJMat(ROOM_HANDLER, 997, "JitsuMat", "Card Jitsu Mat", 3, False, False, None)
-    gameMat.waddle = 997
-    gameMat.game = CardJitsuFireSenseiGame
-    senseiRoom = client['room']
+    game_mat = CJMat(
+        room_handler, 997, "JitsuMat", "Card Jitsu Mat", 3, False, False, None
+    )
+    game_mat.waddle = 997
+    game_mat.game = CardJitsuFireSenseiGame
+    sensei_room = client["room"]
 
     sensei = getSensei(client.engine)
+    game_mat.append(sensei)
+    game_mat.append(client)
 
-    gameMat.append(sensei)
-    gameMat.append(client)
-
-    game = client['game']
-    game.send('scard', game.ext_id, 997, 2)
+    game = client["game"]
+    game.send("scard", game.ext_id, 997, 2)
 
     sensei.penguin.game_index = 0
     game.joinGame(sensei)
-    list.remove(senseiRoom, client)
+    list.remove(sensei_room, client)
 
-@GeneralEvent.on('add-item:8006')
+
+@GeneralEvent.on("add-item:8006")
 @inlineCallbacks
 def AddFireStarterDeck(client):
-    [(yield client.addItem(i)) for i in [821, 3032]]
-    yield client['RefreshHandler'].forceRefresh()
+    # Python 3 forbids yield inside comprehensions.
+    for item_id in (821, 3032):
+        yield client.addItem(item_id)
 
-    cardsToAdd = [client.engine.cardCrumbs[k] for k in FIRE_STARTER_DECK + [choice([250,250,250,250,352])]]
+    yield client["RefreshHandler"].forceRefresh()
 
-    for c in cardsToAdd:
-        if c is None:
+    bonus_card = choice([250, 250, 250, 250, 352])
+    cards_to_add = [
+        client.engine.cardCrumbs[card_id]
+        for card_id in FIRE_STARTER_DECK + [bonus_card]
+    ]
+
+    for card in cards_to_add:
+        if card is None:
             continue
+        if card.id not in client["ninjaHandler"].cards:
+            client["ninjaHandler"].cards[card.id] = [card, 0]
+        client["ninjaHandler"].cards[card.id][1] += 1
 
-        if c.id not in client['ninjaHandler'].cards:
-            client['ninjaHandler'].cards[c.id] = [c, 0]
-
-        client['ninjaHandler'].cards[c.id][1] += 1
-
-    client['ninjaHandler'].ninja.cards = '|'.join(map(lambda x: "{},{}".format(x, client['ninjaHandler'].cards[x][1]), client['ninjaHandler'].cards))
-    client['ninjaHandler'].ninja.save()
+    client["ninjaHandler"].ninja.cards = "|".join(
+        "{},{}".format(card_id, client["ninjaHandler"].cards[card_id][1])
+        for card_id in client["ninjaHandler"].cards
+    )
+    yield client["ninjaHandler"].ninja.save()
